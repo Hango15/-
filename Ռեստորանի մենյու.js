2eeeaeecb0de63e573bfd9fe0800849b3a772636
Hango15 <hovikmanuik@gmail.com>
2024-12-16 13:50:45 +0400

    Add files via upload

---
+++ Ռեստորանի մենյու.js
@@ -0,0 +1,19 @@
+var food = prompt("Ասեք ձեր պատվերը");
+var ord;
+switch (food) {
+    case "Պիցա":
+        ord = document.write("200դրամ");
+        break;
+    case "Պաղպաղակ":
+        ord = document.write("150դրամ");
+        break;
+    case "Ապուր":
+        ord = document.write("300դրամ");
+        break;
+    case "Սուրճ":
+        ord = document.write("100դրամ");
+        break;
+    default:
+        ord = document.write("այդպիսի ապրանք չունենք");
+}
+
